fc971ce1055f0c4fe3ff34774a5d728441354bf8
koenswings <71164441+koenswings@users.noreply.github.com>
2023-02-28 08:55:00 +0100

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App: Component = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Solid
+          Learn Solid the proper way
         </a>
       </header>
     </div>
